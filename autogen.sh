f9b4d11e0fbfc9cd04e90f845f2795e58be3ade7
Eric McConville <erichasemail@gmail.com>
2012-08-29 16:27:31 -0500

    Cleaned vars, and attempted to create a more elegant flow

--- autogen.sh
+++ autogen.sh
@@ -40,13 +40,22 @@ identify_default_binary() {
 # Assume GNU version is last word on first line
 # Format version. ie. '1.10-p8' => '1.10.8'
 identify_version() {
-	VERSION=`$1 --version | head -n 1 | awk '{print $NF}' | sed 's/[^0-9]/./g' | sed 's/\.\././g'`
+	echo `$1 --version | head -n 1 | awk '{print $NF}' | sed 's/[^0-9]/./g' | sed 's/\.\././g'`
+	return 0
 }
 
 # Compare program version to min version
 check_version() {
-	VERSION_OK=""
+	VERSION_ERROR=1
+	if test x$1 = x; then
+		echo "Unable to resolve version number"
+		exit 1
+	fi
     current_version=$1
+	if test x$2 = x; then
+		echo "Unable to evaluate version numbers...missing \$min_version"
+		exit 1
+	fi
     min_version=$2
 	
 	current_version_major="`echo $current_version | cut -d. -f1`"
@@ -62,33 +71,31 @@ check_version() {
 	if test x$min_version_minor = x; then min_version_minor=0 ; fi
 	if test x$min_version_patch = x; then min_version_patch=0 ; fi
 	
-	
 	if [ $min_version_major -lt $current_version_major ] ; then
-		VERSION_OK="true"
+		VERSION_ERROR=0
     elif [ $min_version_major -eq $current_version_major ] ; then
 	  if [ $min_version_minor -lt $current_version_minor ] ; then
-	    VERSION_OK="true"
+	    VERSION_ERROR=0
 	  elif [ $min_version_minor -eq $current_version_minor ] ; then
 	    if [ $min_version_patch -lt $current_version_patch ] ; then
-		VERSION_OK="true"
+			VERSION_ERROR=0
 	    elif [ $min_version_patch -eq $current_version_patch ] ; then
-		VERSION_OK="true"
+			VERSION_ERROR=0
 	    fi
 	  fi
     fi
+    return $VERSION_ERROR;
 }
 
-echo "aclocal..."
+echo "aclocal......\c"
 if test x$ACLOCAL = x; then
   ACLOCAL=`identify_default_binary aclocal`
-  if test x$ACLOCAL = x; then
+  if [ $? -ne 0 ]; then
     die "Did not find a supported aclocal"
   else
-	# Look up & set VERSION
-	identify_version $ACLOCAL
-	# Compare versions
+	VERSION=`identify_version $ACLOCAL`
 	check_version $VERSION $ACLOCAL_MIN_VERSION
-	if test x$VERSION_OK = x; then
+	if [ $? -ne 0 ]; then
 		ACLOCAL=`locate_legacy_binary aclocal-1.7 aclocal17 aclocal-1.5 aclocal15`
 		if test x$ACLOCAL = x; then
 			die "Did not find a supported aclocal"
@@ -97,22 +104,23 @@ if test x$ACLOCAL = x; then
   fi
 fi
 $ACLOCAL || exit 1
+echo "OK"
 
-echo "autoheader..."
+echo "autoheader...\c"
 AUTOHEADER=${AUTOHEADER:-autoheader}
 $AUTOHEADER || exit 1
+echo "OK" 
 
-echo "automake..."
+
+echo "automake.....\c"
 if test x$AUTOMAKE = x; then
   AUTOMAKE=`identify_default_binary automake`
-  if test x$AUTOMAKE = x; then
+  if [ $? -ne 0 ]; then
     die "Did not find a supported automake"
   else
-	# Look up & set VERSION
-	identify_version $AUTOMAKE
-	# compare versions
+	VERSION=`identify_version $AUTOMAKE`
 	check_version $VERSION $AUTOMAKE_MIN_VERSION
-	if test x$VERSION_OK = x; then
+	if [ $? -ne 0 ]; then
 		AUTOMAKE=`locate_legacy_binary automake-1.7`
 		if test x$AUTOMAKE = x; then
 		    die "Did not find a supported automake"
@@ -121,8 +129,10 @@ if test x$AUTOMAKE = x; then
   fi
 fi
 $AUTOMAKE --foreign --add-missing || $AUTOMAKE --gnu --add-missing || exit 1
+echo "OK"
 
-echo "autoconf..."
+
+echo "autoconf.....\c"
 AUTOCONF=${AUTOCONF:-autoconf}
 $AUTOCONF || exit 1
-
+echo "OK"
